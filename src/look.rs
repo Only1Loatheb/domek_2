b7cb9ff0b8a144dd06e28119b604cbac65f4a063
Tomasz Zieliński <zielinski.x360@gmail.com>
2025-03-15 11:17:49 +0100

    movement

---
+++ src/look.rs
@@ -0,0 +1,64 @@
+use bevy::math::{EulerRot, Quat, Vec2};
+use bevy::prelude::{Component, Deref, DerefMut, Query, Res, Transform, With};
+
+use std::f32::consts::FRAC_PI_2;
+
+use bevy::{
+    color::palettes::tailwind, input::mouse::AccumulatedMouseMotion, pbr::NotShadowCaster,
+    prelude::*, render::view::RenderLayers,
+};
+
+
+// https://bevyengine.org/examples/camera/first-person-view-model/
+
+#[derive(Debug, Component, Deref, DerefMut)]
+pub struct CameraSensitivity(Vec2);
+
+impl Default for CameraSensitivity {
+    fn default() -> Self {
+        Self(
+            // These factors are just arbitrary mouse sensitivity values.
+            // It's often nicer to have a faster horizontal sensitivity than vertical.
+            // We use a component for them so that we can make them user-configurable at runtime
+            // for accessibility reasons.
+            // It also allows you to inspect them in an editor if you `Reflect` the component.
+            Vec2::new(0.003, 0.002),
+        )
+    }
+}
+
+
+pub fn look(
+    accumulated_mouse_motion: Res<AccumulatedMouseMotion>,
+    mut player: Query<(&mut Transform, &CameraSensitivity), With<Camera3d>>,
+) {
+    let Ok((mut transform, camera_sensitivity)) = player.get_single_mut() else {
+        return;
+    };
+    let delta = accumulated_mouse_motion.delta;
+
+    if delta != Vec2::ZERO {
+        // Note that we are not multiplying by delta_time here.
+        // The reason is that for mouse movement, we already get the full movement that happened since the last frame.
+        // This means that if we multiply by delta_time, we will get a smaller rotation than intended by the user.
+        // This situation is reversed when reading e.g. analog input from a gamepad however, where the same rules
+        // as for keyboard input apply. Such an input should be multiplied by delta_time to get the intended rotation
+        // independent of the framerate.
+        let delta_yaw = -delta.x * camera_sensitivity.x;
+        let delta_pitch = -delta.y * camera_sensitivity.y;
+
+        let (yaw, pitch, roll) = transform.rotation.to_euler(EulerRot::YXZ);
+        let yaw = yaw + delta_yaw;
+
+        // If the pitch was ±¹⁄₂ π, the camera would look straight up or down.
+        // When the user wants to move the camera back to the horizon, which way should the camera face?
+        // The camera has no way of knowing what direction was "forward" before landing in that extreme position,
+        // so the direction picked will for all intents and purposes be arbitrary.
+        // Another issue is that for mathematical reasons, the yaw will effectively be flipped when the pitch is at the extremes.
+        // To not run into these issues, we clamp the pitch to a safe range.
+        const PITCH_LIMIT: f32 = FRAC_PI_2 - 0.01;
+        let pitch = (pitch + delta_pitch).clamp(-PITCH_LIMIT, PITCH_LIMIT);
+
+        transform.rotation = Quat::from_euler(EulerRot::YXZ, yaw, pitch, roll);
+    }
+}
---
+++ src/movement.rs
@@ -0,0 +1,27 @@
+use bevy::ecs::query::QuerySingleError;
+use bevy::prelude::*;
+
+const SPEED: f32 = 1.0;
+
+pub fn movement(input: Res<ButtonInput<KeyCode>>, timer: Res<Time>, mut camera_transform: Query<&mut Transform, With<Camera3d>>) {
+  let mut movement = Vec3::ZERO;
+  let mut transform = camera_transform.single_mut();
+  let forward = transform.forward().as_vec3();
+  let right = transform.right().as_vec3();
+  if input.pressed(KeyCode::KeyW) {
+    movement += forward;
+  }
+  if input.pressed(KeyCode::KeyS) {
+    movement -= forward;
+  }
+  if input.pressed(KeyCode::KeyA) {
+    movement -= right;
+  }
+  if input.pressed(KeyCode::KeyD) {
+    movement += right;
+  }
+  
+  movement.y = 0.0;
+  movement = SPEED * timer.delta_secs() * movement.normalize_or_zero();
+  transform.translation += movement;
+}
